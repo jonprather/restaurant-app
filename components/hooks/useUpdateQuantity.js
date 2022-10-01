4431e75ed855815c43aef7f7a4f15b9db9cc070d
Jon <jprather.dev@gmail.com>
2022-10-01 16:43:27 -0700

    added optimistic updates to cart quantity

--- components/hooks/useUpdateQuantity.js
+++ components/hooks/useUpdateQuantity.js
@@ -1,9 +1,11 @@
-import React from "react";
 import { useMutation } from "react-query";
 import commerce from "@/lib/commerce";
 import { toast } from "react-toastify";
 import { useQueryClient } from "react-query";
 //note not using error mesgs from Chec
+//Could move this to utility script file but its only used here
+import { createFormattedPriceObj } from "@/util/cartMutationHelpers";
+
 export default function useUpdateQuantity() {
   const queryClient = useQueryClient();
 
@@ -13,22 +15,44 @@ export default function useUpdateQuantity() {
   }
 
   const { mutate } = useMutation(handleQuantityUpdate, {
+    onMutate: async (lineItem) => {
+      //Optimistic Updating of quantity, line_total and subtotal
+      await queryClient.cancelQueries(["cart"]);
+      const previous = queryClient.getQueryData(["cart"]);
+      const updatedCartData = Object.assign({}, previous);
+      const [itemToUpdate] = updatedCartData.line_items.filter(
+        (item) => item.id === lineItem.id
+      );
+      //so its mutating the reference but since its a top level property and I copied the obj its cool
+      itemToUpdate.quantity = lineItem.quantity;
+
+      const rawPrice = (itemToUpdate.price.raw * itemToUpdate.quantity).toFixed(
+        2
+      );
+
+      const lineTotalPriceFormat = createFormattedPriceObj(rawPrice);
+      itemToUpdate.line_total = lineTotalPriceFormat;
+
+      let sum = updatedCartData.line_items
+        .reduce((acc, item) => {
+          return acc + +item.line_total.raw;
+        }, 0)
+        .toFixed(2);
+
+      const subTotalPriceFormat = createFormattedPriceObj(sum);
+      updatedCartData.subtotal = subTotalPriceFormat;
+
+      queryClient.setQueryData(["cart"], updatedCartData);
+      return { previous, updatedCartData };
+    },
+    onError: (err, updatedCartData, context) => {
+      toast.error(err.message, { toastId: err.message });
+      queryClient.setQueryData(["cart"], context.previous);
+    },
     onSuccess: async (product) => {
-      // const text = isIncrement ? "Increased" : "decresed";
-      // toast.success(`${text} qauntity for ${product.product_name}!`, {
-      //   toastId: text + product.product_id,
-      // });
-      // Could make more specific to jus tthis cart item right via
-      //query key which includes ids?
-      queryClient.invalidateQueries(["cart"], {
-        refetchActive: true,
-      });
+      queryClient.invalidateQueries(["cart"]);
     },
   });
 
   return mutate;
 }
-//COULD make this optimistic updates or atlest setQuery CACHE
-// this works but is pretty slow so maybe optimistic updates would be better
-
-//TODO ok this works but do i need to invalidate queries or anything it seems to work as it is
