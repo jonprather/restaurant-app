7236a4452d018f9767741322688736358b7aa501
Jon <jprather.dev@gmail.com>
2022-10-10 17:00:29 -0700

    added form validation to newsletter input

--- components/organisms/Newsletter.js
+++ components/organisms/Newsletter.js
@@ -1,4 +1,44 @@
+import React, { useState } from "react";
+import { z } from "zod";
+import { getZodErrorMessage } from "@/lib/zodHelpers";
+import { toast } from "react-toastify";
+
 export default function Newsletter() {
+  const [state, setState] = useState({
+    email: "",
+  });
+
+  const [touched, setTouched] = useState({
+    email: false,
+  });
+  const [errorsObj, setErrorsObj] = useState({
+    email: false,
+  });
+  const resetState = () => {
+    setState({
+      email: "",
+    });
+    setTouched({
+      email: false,
+    });
+  };
+  const FormSchema = z.object({
+    email: z.string().email({ message: "Invalid email address" }),
+  });
+
+  const handleSubmit = () => {
+    Object.entries(state).forEach(([key, value]) => {
+      const errorMsg = getZodErrorMessage(FormSchema.shape[key], value);
+      setErrorsObj((prev) => {
+        return { ...prev, [key]: errorMsg };
+      });
+    });
+
+    if (FormSchema.safeParse({ ...state }).success) {
+      toast.success(`You have been added to our subscriber list!`);
+      resetState();
+    }
+  };
   return (
     <section class='newsletter'>
       <div class='newsletter-container'>
@@ -18,11 +58,34 @@ export default function Newsletter() {
             autofill='true'
             type='email'
             placeholder='shakir260@gmail.com'
+            value={state.email}
+            onChange={(e) => {
+              setErrorsObj((prev) => {
+                return { ...prev, email: null };
+              });
+              setState((prev) => {
+                return { ...prev, email: e.target.value };
+              });
+            }}
+            onBlur={(e) => {
+              const errorMsg = getZodErrorMessage(
+                FormSchema.shape.email,
+                e.target.value
+              );
+              setErrorsObj((prev) => {
+                return { ...prev, email: errorMsg };
+              });
+            }}
           />
-          <button type='submit' class='newsletter__input-container-button'>
+          <button
+            onClick={handleSubmit}
+            type='submit'
+            class='newsletter__input-container-button'
+          >
             Subscribe
           </button>
         </div>
+        <p className='text-red-600 text-xl italic pt-2'>{errorsObj.email}</p>
       </div>
     </section>
   );
