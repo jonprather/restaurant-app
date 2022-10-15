4d94d26c4ea6257c46152fe914638febbd99c9b5
Jon <jprather.dev@gmail.com>
2022-10-14 17:02:46 -0700

    refactored header layout

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ export default function Header() {
         <div className='header__hero--small'>
           <img
             className=''
-            src='/images/hero-small.jpg'
+            src='/images/hero-cropped.png'
             alt='kebab menu item'
           />
         </div>
